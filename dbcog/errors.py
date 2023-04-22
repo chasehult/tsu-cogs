3f1f7f9d3d8ba87dfcda94e5432027a21f3ccae8
River <18037011+RheingoldRiver@users.noreply.github.com>
2023-04-22 01:08:35 -0500

    test commit (#1)

--- dbcog/errors.py
+++ dbcog/errors.py
@@ -10,7 +10,7 @@ class InvalidMonsterId(KeyError):
         self.monster_id = monster_id
 
     def __str__(self):
-        return '{} is not found in the graph'.format(str(self.monster_id))
+        return "{} is not found in the graph".format(str(self.monster_id))
 
 
 class QueryFailure(OperationalError):
