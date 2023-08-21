5090a8b1c9057b9b2a2fd42917ba6426eead28ca
AA <chasehult@gmail.com>
2023-08-20 17:04:34 -0700

    Fix ID Range
    Most useless token ever

--- dbcog/find_monster/tokens/find_monster_tokens.py
+++ dbcog/find_monster/tokens/find_monster_tokens.py
@@ -613,7 +613,8 @@ def normalize_score(score: float, max_score: float) -> float:
     return score / max_score
 
 
-SPECIAL_TOKEN_TYPES: Set[Type[SpecialToken]] = {
+SPECIAL_TOKEN_TYPES: List[Type[SpecialToken]] = [
+    IDRangeToken,  # Needs to be before MultipleAwakeningToken
     MultipleAwakeningToken,
     MonsterAttributeNumeric,
     MonsterAttributeString,
@@ -625,5 +626,4 @@ SPECIAL_TOKEN_TYPES: Set[Type[SpecialToken]] = {
     SingleAttributeToken,
     MultiAttributeToken,
     OrToken,
-    IDRangeToken,
-}
+]
